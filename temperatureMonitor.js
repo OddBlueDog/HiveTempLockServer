efe08fcecfe2200d3121cb9f1365f42ec02fb803
thomas <thomas01155@gmail.com>
2020-12-10 16:14:02 +0000

    logging

--- temperatureMonitor.js
+++ temperatureMonitor.js
@@ -1,14 +1,14 @@
-const pool = require("./db");
-const User = require("./class/User");
+const pool = require('./db');
+const User = require('./class/User');
 
 async function getAllTemps() {
-  return await pool.query("SELECT email,password,maxTemp,tempToSet FROM users");
+  return await pool.query('SELECT email,password,maxTemp,tempToSet FROM users');
 }
 
-module.exports.loop = async function() {
-  console.log("Starting loop scan");
+module.exports.loop = async function () {
+  console.log('Starting loop scan');
   const allTemps = await getAllTemps();
-  Object.keys(allTemps).forEach(async function(key) {
+  Object.keys(allTemps).forEach(async function (key) {
     try {
       const row = allTemps[key];
       const user = new User(row.email, row.password);
@@ -17,9 +17,8 @@ module.exports.loop = async function() {
       // If current target temp is above user specified max target temperature then set the temperature
       // to the temperature the user has specified as their default
 
-      console.log('there are this many thermostats',  user.thermostats.length);
-      user.thermostats.forEach(thermostat => {
-        
+      console.log('there are this many thermostats', user.thermostats.length);
+      user.thermostats.forEach((thermostat) => {
         if (
           user.getCurrentTargetTemp(thermostat) > row.maxTemp ||
           user.getCurrentReportedValue(thermostat) > row.maxTemp
@@ -28,16 +27,17 @@ module.exports.loop = async function() {
         }
 
         console.log(
-          `Current target temp ${user.getCurrentTargetTemp(thermostat)} Reported value: ${user.getCurrentReportedValue(thermostat)} Desired max temp ${
-            row.maxTemp
-          } Temperature to set if above max ${row.tempToSet}`
+          `Current target temp ${user.getCurrentTargetTemp(
+            thermostat
+          )} Reported value: ${user.getCurrentReportedValue(
+            thermostat
+          )} Desired max temp ${row.maxTemp} Temperature to set if above max ${
+            row.tempToSet
+          }`
         );
-  
-      })
-
- 
+      });
     } catch (e) {
-      console.log("An issue occured");
+      console.log('An issue occured ', e);
     }
   });
 };
